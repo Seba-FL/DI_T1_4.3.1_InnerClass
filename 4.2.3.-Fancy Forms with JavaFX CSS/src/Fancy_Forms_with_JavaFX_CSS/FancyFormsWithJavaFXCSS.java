e7a5b77a07dbd4bbda72ff44d8abb0fb8937d373
Sebastián <65761313+Seba-FL@users.noreply.github.com>
2020-10-18 11:51:53 +0200

    Add files via upload

---
+++ 4.2.3.-Fancy Forms with JavaFX CSS/src/Fancy_Forms_with_JavaFX_CSS/FancyFormsWithJavaFXCSS.java
@@ -0,0 +1,98 @@
+/*
+ *
+ * Autor: Sebastián Fernández López
+ * Descripción: Actividad 4.2.2 (Creating a Form in JavaFX)
+ *   
+ */
+package Fancy_Forms_with_JavaFX_CSS;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+
+public class FancyFormsWithJavaFXCSS extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("JavaFX Welcome (Bienvenido a JavaFX)");
+        primaryStage.show();
+
+        /**
+         * Creamos ahora el diseño de nuestra ventana a mostrar
+         */
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+
+        Scene scene = new Scene(grid, 300, 275);
+        primaryStage.setScene(scene);
+        scene.getStylesheets().add("source/Login.css");
+        primaryStage.show();
+
+        /**
+         * Añadimos las etiquetas necesarias para dar la bienvenida
+         */
+        Text scenetitle = new Text("Welcome");
+        scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        grid.add(scenetitle, 0, 0, 2, 1);
+
+        Label userName = new Label("User Name");
+        grid.add(userName, 0, 1);
+
+        TextField userTextField = new TextField();
+        grid.add(userTextField, 1, 1);
+
+        Label pw = new Label("Password");
+        grid.add(pw, 0, 2);
+
+        PasswordField pwBox = new PasswordField();
+        grid.add(pwBox, 1, 2);
+
+        /**
+         *
+         * A continuación añadimos el botón que hace que accedamos a nuestra
+         * plataforma
+         *
+         */
+        Button btn = new Button("Sign in");
+        HBox hbBtn = new HBox(10);
+
+        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+        hbBtn.getChildren().add(btn);
+        grid.add(hbBtn, 1, 4);
+
+        final Text actiontarget = new Text();
+        grid.add(actiontarget, 1, 6);
+
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+
+            /**
+             *
+             * Ponemos el código para que nos muestre un mensaje al presionar el
+             * botón mostando el mensaje de "Botón de inicio presionado"
+             *
+             */
+            @Override
+            public void handle(ActionEvent e) {
+                actiontarget.setFill(Color.FIREBRICK);
+                actiontarget.setText("Sign in button pressed");
+            }
+        });
+    }
+}
